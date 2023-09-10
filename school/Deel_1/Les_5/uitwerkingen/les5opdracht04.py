f103301673797f76120a4a22b037df92f8818a71
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-09-10 11:06:24 +0200

    verder gewerkt

---
+++ school/Deel_1/Les_5/uitwerkingen/les5opdracht04.py
@@ -0,0 +1,6 @@
+straal = input("wat de straal?")
+pi = 3.14159
+uitkomst = input(straal * straal * pi)
+uitkomst = "onbekent"
+if straal * straal * pi:
+    print(f"De oppervlakte van een cirkel met {straal} is {uitkomst}")
