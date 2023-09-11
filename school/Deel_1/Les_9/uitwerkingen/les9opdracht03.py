dfe2f92c29a5f027aed4d3a66e30ade50bc501ce
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-09-11 13:12:59 +0200

    .

--- school/Deel_1/Les_9/uitwerkingen/les9opdracht03.py
+++ school/Deel_1/Les_9/uitwerkingen/les9opdracht03.py
@@ -4,7 +4,8 @@ diploma = "heb je een diploma?"
 int(input(diploma))
 leeftijd = "wat is je leeftijd?"
 int(input(leeftijd))
-if leeftijd==18: 
+if leeftijd==18 and snor=="ja" and diploma=="ja": 
     print(f"Gefeliciteerd, je bent aangenomen!")
 elif  leeftijd<18:
-    print(f"Gefeliciteerd, je bent aangenomen!")         
+    print(f"Gefeliciteerd, je bent aangenomen!")   
+#          
