3130379c6dc68ed27812176773e60032dc2b80fd
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-09-11 13:13:27 +0200

    .

--- school/Deel_1/Les_9/uitwerkingen/les9opdracht03.py
+++ school/Deel_1/Les_9/uitwerkingen/les9opdracht03.py
@@ -1,11 +1,10 @@
-snor = "heb je een snor?"
-int(input(snor))
+snor = int(input("heb je een snor?"))
 diploma = "heb je een diploma?"
-int(input(diploma))
+diploma = int(input(diploma))
 leeftijd = "wat is je leeftijd?"
 int(input(leeftijd))
 if leeftijd==18 and snor=="ja" and diploma=="ja": 
     print(f"Gefeliciteerd, je bent aangenomen!")
 elif  leeftijd<18:
     print(f"Gefeliciteerd, je bent aangenomen!")   
-#          
+#    
