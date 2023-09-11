16ff709edb5972be73d2454e91979da4b04708b7
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-09-11 14:08:57 +0200

    opdracht afgemaakt

--- school/Deel_1/Les_9/uitwerkingen/les9opdracht03.py
+++ school/Deel_1/Les_9/uitwerkingen/les9opdracht03.py
@@ -1,10 +1,20 @@
-snor = int(input("heb je een snor?"))
-diploma = "heb je een diploma?"
-diploma = int(input(diploma))
-leeftijd = "wat is je leeftijd?"
-int(input(leeftijd))
-if leeftijd==18 and snor=="ja" and diploma=="ja": 
+snor = (input(f"heb je een snor?"))
+
+
+
+diploma = (input(f"heb je een diploma?"))
+
+
+   
+
+leeftijd = (input(f"wat is je leeftijd?"))
+
+if leeftijd=="18" and snor=="ja" and diploma=="ja": 
     print(f"Gefeliciteerd, je bent aangenomen!")
-elif  leeftijd<18:
-    print(f"Gefeliciteerd, je bent aangenomen!")   
-#    
+elif leeftijd>"18" and diploma=="ja":
+    print(f"Gefeliciteerd, je bent aangenomen!")    
+elif leeftijd<"18" and diploma=="ja":
+    print(f"Gefeliciteerd, je bent aangenomen!")  
+elif diploma=="nee":
+    print(f"Helaas, je bent niet aangenomen.")     
+# code werkt nu wel    
