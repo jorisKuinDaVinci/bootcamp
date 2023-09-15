bac2a169daf1c5993a69d00b11b0d62bc31f8332
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-09-15 14:17:17 +0200

    verder gewerkt

--- school/Deel_2/Les_12/uitwerkingen/les12opdracht0101.py
+++ school/Deel_2/Les_12/uitwerkingen/les12opdracht0101.py
@@ -1,1 +1,10 @@
-cijfer = input("wat is je cijfer?")
+name = input('Wat is jouw naam? ')
+print('Hallo', name)
+leeftijd = input('Wat is je leeftijd? ')
+print('Hallo', name, 'je leeftijd is', leeftijd)
+
+def telop (leeftijd, getal2):
+    resultaat= leeftijd + getal2
+    return resultaat
+
+print(telop(leeftijd,5))
--- school/Deel_2/Les_15/uitwerkingen/les15test06.py
+++ school/Deel_2/Les_15/uitwerkingen/les15test06.py
@@ -3,8 +3,20 @@ def divide (getal1, getal2):
     result = getal1 / getal2
     return result
 
+def multiply (getal1, getal2):
+    result = getal1 * getal2
+    return result
+
+def operate (getal1, getal2, operator):
+    if op == '/':
+        return divide(g1,g2)
+    elif op == 
+
 
 g1 = int(input('getal1? '))
 g2 = int(input('getal2? '))
-
-print(  divide(g1, g2)  )
+op = input('operator?' )
+if op == '/':
+    print(divide(g1,g2))
+elif op == '*':
+    print(multiply(g1,g2))
